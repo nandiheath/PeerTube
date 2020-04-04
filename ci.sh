c4c0679fbe2733ff7b3dd82a709652cbf331c6b6
Nandiheath <nandi0315@gmail.com>
2020-04-04 09:03:49 +0800

    remove pcc64le

--- ci.sh
+++ ci.sh
@@ -4,14 +4,16 @@
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "tvhk-dev" ]; then
   docker buildx build \
     --progress plain \
-    --platform=linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le,linux/s390x \
+    # ,linux/ppc64le,linux/s390x -> some libraries not avaliable on pcc64le
+    --platform=linux/amd64,linux/arm64,linux/arm/v7 \ 
     .
   exit $?
 fi
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin &> /dev/null
 docker buildx build \
      --progress plain \
-    --platform=linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le,linux/s390x \
+    --platform=linux/amd64,linux/arm64,linux/arm/v7 \
+    # ,linux/ppc64le,linux/s390x -> some libraries not avaliable on pcc64le
     -t nandiheath/tvhk-peertube:$TRAVIS_COMMIT \
     --build-arg NPM_RUN_BUILD_OPTS=$NPM_RUN_BUILD_OPTS \
     --push \
